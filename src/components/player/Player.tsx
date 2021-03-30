189d889b2cc52451765e862381249bed66e6f326
tim0-12432 <timo.kapellner@gmail.com>
2021-03-30 20:36:06 +0200

    fixed for deploy

--- src/components/player/Player.tsx
+++ src/components/player/Player.tsx
@@ -18,22 +18,23 @@ const Player = ({ position, setPosition }: any) => {
         jump,
     } = useKeyboardControls();
       
-    const [ref, api] = useSphere(() => ({
-        mass: 1,
-        type: "Dynamic",
-        position: position
-    }));
+    const ref = useRef(position);
+    //const [ref, api] = useSphere(() => ({
+    //    mass: 1,
+    //    type: "Dynamic",
+    //    position: position
+    //}));
 
-    const velocity = useRef([0, 0, 0]);
-    useEffect(() => {
-        api.velocity.subscribe((v) => (velocity.current = v));
-    }, [api.velocity]);
+    //const velocity = useRef([0, 0, 0]);
+    //useEffect(() => {
+    //    api.velocity.subscribe((v) => (velocity.current = v));
+    //}, [api.velocity]);
 
     useFrame(() => {
             camera.position.copy(new Vector3(
-                ref.current !== undefined ? ref.current.position.x : 0,
-                ref.current !== undefined ? ref.current.position.y + 1 : 0,
-                ref.current !== undefined ? ref.current.position.z : 0
+                //ref.current !== undefined ? ref.current.position.x : 0,
+                //ref.current !== undefined ? ref.current.position.y + 1 : 0,
+                //ref.current !== undefined ? ref.current.position.z : 0
         ));
 
         const frontVector = new Vector3(
@@ -50,11 +51,11 @@ const Player = ({ position, setPosition }: any) => {
             .multiplyScalar(SPEED)
             .applyEuler(camera.rotation);
         
-        api.velocity.set(direction.x, velocity.current[1], direction.z);
+        //api.velocity.set(direction.x, velocity.current[1], direction.z);
 
-        if (jump && (Math.abs(Number(velocity.current[1].toFixed(2))) < 0.05)) {
-            api.velocity.set(velocity.current[0], 8, velocity.current[2]);
-        }
+        //if (jump && (Math.abs(Number(velocity.current[1].toFixed(2))) < 0.05)) {
+        //    api.velocity.set(velocity.current[0], 8, velocity.current[2]);
+        //}
     });
 
     return (
