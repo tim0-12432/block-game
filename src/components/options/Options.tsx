6c5101fd19fd880ba4261d22ec7831ee811de91d
tim0-12432 <timo.kapellner@gmail.com>
2021-04-25 16:15:27 +0200

    fixed settings issues

--- src/components/options/Options.tsx
+++ src/components/options/Options.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect, useState } from "react";
+import React, { FC, useState } from "react";
 import { OptionsProps } from "../../types";
 
 import styles from "./Options.module.scss";
@@ -9,6 +9,7 @@ interface Props {
 }
 
 const Options: FC<Props> = ({ options, setOptions }: Props) => {
+	const [open, setOpen] = useState(false);
 
 	function toggleOption(key: string) {
 		setOptions({
@@ -19,13 +20,15 @@ const Options: FC<Props> = ({ options, setOptions }: Props) => {
 
 	return (
 		<div className={ styles.options }>
-			<h2>Settings</h2>
+			<h2 onClick={ (e) => setOpen(!open) } style={open ? {border: "2px solid transparent"} : {border: "2px solid white"}}>Settings</h2>
 			{
-				Object.keys(options).map((key) => {
-					return (
-						<p key={ key }><b>{ key }:</b> { options[key] ? <span className={ styles.on } onClick={ (e) => toggleOption(key) }><i className="far fa-check-square" /></span> : <span className={ styles.off } onClick={ (e) => toggleOption(key) }><i className="far fa-square" /></span> }</p>
-					);
-				})
+				open
+					? Object.keys(options).map((key) => {
+						return (
+							<p key={ key }><b>{ key }:</b> { options[key] ? <span className={ styles.on } onClick={ (e) => toggleOption(key) }><i className="far fa-check-square" /></span> : <span className={ styles.off } onClick={ (e) => toggleOption(key) }><i className="far fa-square" /></span> }</p>
+						);
+					})
+					: null
 			}
 		</div>
 	);
